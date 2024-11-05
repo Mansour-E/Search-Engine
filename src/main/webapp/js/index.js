2d45c5f06fa083ecfb4976ff28ab769278b94eb2
yassine77 <kchaou.yessin@gmail.com>
2024-11-05 23:28:15 +0100

    Change functionality to extract and split search terms from input field

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,42 +1,5 @@
 $(function() {
-    const searchTerms = [];
-    const searchSites = [];
-    const searchResult =  document.getElementById('searchResult');
-    const siteResult =  document.getElementById('siteResult');
 
-
-    $("#searchQuery").on('keydown', (e) => {
-      if (e.key === ' ' || e.key === 'Enter') {
-          e.preventDefault();
-          let input = e.target.value.trim();
-          if (input) {
-              searchTerms.push(input);
-              e.target.value = '';
-              const liTag = document.createElement("li");
-              const textnode = document.createTextNode(input);
-              liTag.appendChild(textnode);
-              searchResult.appendChild(liTag);
-              console.log('Current search terms:', searchTerms);
-
-          }
-      }
-    });
-     $("#siteQuery").on('keydown', (e) => {
-      if (e.key === ' ' || e.key === 'Enter') {
-          e.preventDefault();
-          let input = e.target.value.trim();
-          if (input) {
-              searchSites.push(input);
-              e.target.value = '';
-              const liTag = document.createElement("li");
-              const textnode = document.createTextNode(input);
-              liTag.appendChild(textnode);
-              siteResult.appendChild(liTag);
-              console.log('Current search terms:', searchSites);
-
-          }
-      }
-    });
 
     function displayResults(response) {
         console.log("displayResults", response)
@@ -67,9 +30,10 @@ $(function() {
     $("button").on("click", function(event) {
         event.preventDefault()
         const k = 20;
+
         const query = {
-            searchTerms: searchTerms,
-            domainSiteTerms: searchSites,
+            searchTerms: $("#searchQuery").val().trim().split(/\s+/),
+            domainSiteTerms: $("#siteQuery").val().trim().split(/\s+/),
             isConjunctive: $("#conjunctiveCheck").is(":checked")
         };
 
@@ -84,5 +48,6 @@ $(function() {
                 console.log(e)
             }
         })
+
     });
 });
